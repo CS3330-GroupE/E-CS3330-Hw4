6dc7b0d1554bdce68fe328d58e7265471555eb54
bsimpson0 <91200121+bsimpson0@users.noreply.github.com>
2024-02-21 21:04:35 -0600

    Created vehicle subclasses, created vehicle getters and setters

--- HW4/src/edu/mu/vehicleManager/Vehicle.java
+++ HW4/src/edu/mu/vehicleManager/Vehicle.java
@@ -29,6 +29,22 @@ public abstract class Vehicle {
 		this.gasTankCapacity = gasTankCapacity;
 		this.startType = startType;
 	}
+	
+	
+	//copy constructors
+	public Vehicle(Vehicle copy) {
+		this.brand = copy.brand;
+		this.make = copy.make;
+		this.modelYear = copy.modelYear;
+		this.price = copy.price;
+		this.color = copy.color;
+		this.fuelType = copy.fuelType;
+		this.mileage = copy.mileage;
+		this.mass = copy.mass;
+		this.cylinders = copy.cylinders;
+		this.gasTankCapacity = copy.gasTankCapacity;
+		this.startType = copy.startType;
+	}
 
 	@Override
 	public String toString() {
@@ -36,6 +52,94 @@ public abstract class Vehicle {
 				+ ", color=" + color + ", fuelType=" + fuelType + ", mileage=" + mileage + ", mass=" + mass
 				+ ", cylinders=" + cylinders + ", gasTankCapacity=" + gasTankCapacity + ", startType=" + startType
 				+ "]";
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public long getModelYear() {
+		return modelYear;
+	}
+
+	public void setModelYear(long modelYear) {
+		this.modelYear = modelYear;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public VehicleColor getColor() {
+		return color;
+	}
+
+	public void setColor(VehicleColor color) {
+		this.color = color;
+	}
+
+	public FuelType getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(FuelType fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public double getMileage() {
+		return mileage;
+	}
+
+	public void setMileage(double mileage) {
+		this.mileage = mileage;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+
+	public int getCylinders() {
+		return cylinders;
+	}
+
+	public void setCylinders(int cylinders) {
+		this.cylinders = cylinders;
+	}
+
+	public double getGasTankCapacity() {
+		return gasTankCapacity;
+	}
+
+	public void setGasTankCapacity(double gasTankCapacity) {
+		this.gasTankCapacity = gasTankCapacity;
+	}
+
+	public StartMechanism getStartType() {
+		return startType;
+	}
+
+	public void setStartType(StartMechanism startType) {
+		this.startType = startType;
 	}
 	
 	
